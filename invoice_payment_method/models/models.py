a9942a7969f700b57c092bdd9dea15d008534e17
Enrique Jaquez Olvera <enrique.jaquez@sursoom.mx>
2022-06-23 09:55:15 -0600

    update models

--- invoice_payment_method/models/models.py
+++ invoice_payment_method/models/models.py
@@ -1,15 +1,4 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-class Datos_facturas(models.Model):
-
-	_inherit = 'account.move'
-
-	tipo_factura=fields.Selection([('PUE','PUE - Pago en una sola exhibición'),
-	                           ('PPD','PPD - Pago en parcialidades o diferido')])
-
-
-# -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, Warning
 
