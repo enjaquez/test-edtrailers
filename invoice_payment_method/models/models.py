c359d8c5a8afa3a31ecac03d78152d2aa09c6f38
Enrique Jaquez Olvera <enrique.jaquez@sursoom.mx>
2022-06-23 09:45:35 -0600

    update invoice payment method

--- invoice_payment_method/models/models.py
+++ invoice_payment_method/models/models.py
@@ -7,3 +7,24 @@ class Datos_facturas(models.Model):
 
 	tipo_factura=fields.Selection([('PUE','PUE - Pago en una sola exhibición'),
 	                           ('PPD','PPD - Pago en parcialidades o diferido')])
+
+
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, Warning
+
+
+class Datos_facturas(models.Model):
+
+	_inherit = 'account.move'
+
+	tipo_factura=fields.Selection([('PUE','PUE - Pago en una sola exhibición'),
+	                           ('PPD','PPD - Pago en parcialidades o diferido')])
+
+#realizar las validaciones al momento de guardar
+def write(self, values):
+    for move in self:
+        if not move.tipo_factura:
+            raise UserError(_("Seleccione el Metodo de Pago"
+                              "e intente nuevamente."))
+return result
