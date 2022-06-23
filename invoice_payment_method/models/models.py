eb22be4d7776290e8d7ed6ff40f12508fb2817fd
Enrique Jaquez Olvera <enrique.jaquez@sursoom.mx>
2022-06-23 11:49:19 -0600

    update models

--- invoice_payment_method/models/models.py
+++ invoice_payment_method/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo import models, exceptions, fields, api, _
+from odoo.exceptions import UserError, ValidationError
 
 
 class Datos_facturas(models.Model):
@@ -9,9 +9,11 @@ class Datos_facturas(models.Model):
 	tipo_factura=fields.Selection([('PUE','PUE - Pago en una sola exhibición'),
 	                           ('PPD','PPD - Pago en parcialidades o diferido')])
 
-@api.constrains('l10n_mx_edi_payment_policy')
-def _validate_l10n_mx_edi_payment_policy(self):
-    if record in self:
-    	if record.l10n_mx_edi_payment_policy == 'PUE':
-    		raise ValidationError("El método seleccionado es PUE")
+	@api.constrains('l10n_mx_edi_payment_policy')
+	def _validate_l10n_mx_edi_payment_policy(self):
+    	if record in self:
+    		if record.l10n_mx_edi_payment_policy == 'PUE':
+    			raise ValidationError("El método seleccionado es PUE")
+
+
 
