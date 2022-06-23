9d40ae3c43ba48804ff34c43a79d7681e6d474a4
Enrique Jaquez Olvera <enrique.jaquez@sursoom.mx>
2022-06-23 12:29:25 -0600

    update models

--- invoice_payment_method/models/models.py
+++ invoice_payment_method/models/models.py
@@ -11,7 +11,7 @@ class Datos_facturas(models.Model):
 	@api.constrains('tipo_factura','l10n_mx_edi_payment_method_id')
 	def _validate_tipo_factura(self):
 		for record in self:
-			print (ecord.l10n_mx_edi_payment_method_id)
+			print (record.l10n_mx_edi_payment_method_id)
 			if record.tipo_factura == 'PUE' and record.l10n_mx_edi_payment_method_id == '99':
 				raise ValidationError(_('Si el método de pago es PUE, entonces la forma de pago debe ser diferente a 99 - Por definir - Selecciona otras opciones'))
 
