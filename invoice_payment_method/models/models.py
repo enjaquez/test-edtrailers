bbb18b5185c96061c1d0426f0b220938dafe4666
Enrique Jaquez Olvera <enrique.jaquez@sursoom.mx>
2022-06-23 11:24:45 -0600

    update models

--- invoice_payment_method/models/models.py
+++ invoice_payment_method/models/models.py
@@ -9,9 +9,9 @@ class Datos_facturas(models.Model):
 	tipo_factura=fields.Selection([('PUE','PUE - Pago en una sola exhibición'),
 	                           ('PPD','PPD - Pago en parcialidades o diferido')])
 
-	@api.constrains('l10n_mx_edi_payment_policy')
-	def _validate_l10n_mx_edi_payment_policy(self):
-    	if record in self:
-    		if record.l10n_mx_edi_payment_policy == 'PUE':
-    			raise ValidationError("El método seleccionado es PUE")
+@api.constrains('l10n_mx_edi_payment_policy')
+def _validate_l10n_mx_edi_payment_policy(self):
+    if record in self:
+    	if record.l10n_mx_edi_payment_policy == 'PUE':
+    		raise ValidationError("El método seleccionado es PUE")
 
