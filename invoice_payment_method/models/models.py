00596a11efa8dcb0de5fbd7029202c2c895efe9d
Enrique Jaquez Olvera <enrique.jaquez@sursoom.mx>
2022-06-27 12:37:07 -0600

    update models

--- invoice_payment_method/models/models.py
+++ invoice_payment_method/models/models.py
@@ -13,11 +13,12 @@ class Datos_facturas(models.Model):
 	                           ('PPD','PPD - Pago en parcialidades o diferido')], 
 	                           string="Metodo de pago",
 	                           required=True,
-	                           default='PUE')
+	                           default='PPD')
 
 	@api.constrains('tipo_factura','l10n_mx_edi_payment_method_id')
 	def _validate_tipo_factura(self):
 		for record in self:
+			record.invoice_date_due = datetime.datetime.now() + timedelta(days=30)
 			#raise ValidationError (record.partner_id.country_id.name)
 			if record.tipo_factura == 'PUE' and record.l10n_mx_edi_payment_method_id.name == 'Por definir':
 				raise ValidationError(_('Si el método de pago es PUE, entonces la forma de pago debe ser diferente a 99 - Por definir - Selecciona otras opciones'))
